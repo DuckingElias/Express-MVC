cf6e7f425c8ea31d379f464dcd91174a663755ed
Elias Kleppinger <eliaskleppinger@web.de>
2020-11-13 10:31:11 +0100

    Added insert example

--- src/Controller/IndexController.mjs
+++ src/Controller/IndexController.mjs
@@ -5,11 +5,23 @@ export default class IndexController extends Controller {
 
     getLatestArticles = async () => {
         let articles = await Article.findAll({
-            limit: 3
+            limit: 3,
+            order: [
+                ["id", "DESC"]
+            ]
         });
         this.res.render("index", {
             articles: articles
         });
+    }
+
+    postNewArticle = async() => {
+        logger.debug(this.req.body)
+        let article = await Article.create({
+            title:  this.req.body.title,
+            content: this.req.body.content
+        });
+        this.res.redirect("/");
     }
 
 }
--- src/Routes/IndexRoute.mjs
+++ src/Routes/IndexRoute.mjs
@@ -2,8 +2,12 @@ import express from 'express';
 import IndexController from '../Controller/IndexController.mjs';
 
 const router = express.Router();
-router.get("/", (req, res, next) => {
-    new IndexController(req, res).getLatestArticles();
+router.get("/", async (req, res, next) => {
+    await new IndexController(req, res).getLatestArticles();
+});
+
+router.post("/", async (req, res, next) => {
+    await new IndexController(req, res).postNewArticle();
 });
 
 export {
